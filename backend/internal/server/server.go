461880da91a7f306d8b0a003b764d6d612084f84
hongsheng <24836227@qq.com>
2023-05-06 23:12:59 +0800

    增加功能，在服务器初始化时检查配置文件，如果不存在配置文件，创建默认的配置文件

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -1,6 +1,11 @@
 package server
 
 import (
+	"fmt"
+	"oneTicket/backend/internal/config"
+	"os"
+
+	"github.com/toolkits/file"
 	"github.com/toolkits/logger"
 )
 
@@ -9,9 +14,20 @@ func Start() {
 	logger.Infoln("启动服务")
 
 	// 配置初始化
+	if err := config.Read(); err != nil {
+		fmt.Printf("无法读取配置文件，err:%v", err)
+		if !file.IsExist(config.ConfigFileName) {
+			fmt.Printf("配置文件不存在，重新初始化配置文件,请确认配置文件后重新运行")
+			_ = config.New()
+			_ = config.Save()
+			os.Exit(1)
+		}
+	}
 
 	// 日志初始化
 
 	// 数据库初始化
+
+	// 路由初始化
 
 }
